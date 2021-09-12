b1a97564bb005092ab28affb300aaecfdb8c03d8
Rui Xian <tayrx9988@gmail.com>
2021-09-12 11:53:52 +0800

    add amplify and connect to userpool

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css';
+import Amplify from 'aws-amplify';
+import awsconfig from '../src/aws-exports';
 
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient()
+  Amplify.configure(awsconfig)
 
   return (
     <QueryClientProvider client={queryClient}>
