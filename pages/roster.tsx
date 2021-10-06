b3883ee411d266a63aec5d0480aa30fe3e1e637f
Rui Xian <tayrx9988@gmail.com>
2021-10-06 10:53:09 +0800

    add layout to roster

--- pages/roster.tsx
+++ pages/roster.tsx
@@ -1,8 +1,11 @@
+import React from "react"
+import Layout from "../components/Common/Layout"
+
 const Roster = () => {
     return (
-        <div>
-
-        </div>
+        <Layout header="Roster Imum Occupancy 150">
+            
+        </Layout>
     )
 }
 export default Roster
