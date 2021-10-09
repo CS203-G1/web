192a03e5fbf21dceed460a6342ebf70f09d3fb3a
Rui Xian <tayrx9988@gmail.com>
2021-10-09 22:43:25 +0800

    employee page

--- pages/employee/[slug].tsx
+++ pages/employee/[slug].tsx
@@ -1,12 +1,16 @@
 import { useRouter } from 'next/router'
+import React from 'react'
+import Layout from '../../components/Common/Layout'
 const Employee = () => {
     const router = useRouter()
     const { slug } = router.query
 
     return (
-        <div>
-            {slug}
-        </div>
+        <Layout header="Employee No need wash hands">
+            <div>
+                
+            </div>
+        </Layout>
     )
 }
 
