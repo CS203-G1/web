ed094d9aed744801169bc71d31dc458503a62d58
Rui Xian <47887899+moocowdrinksmilk@users.noreply.github.com>
2021-10-10 16:34:48 +0800

    Update Layout.tsx

--- components/Common/Layout.tsx
+++ components/Common/Layout.tsx
@@ -11,11 +11,11 @@ interface props {
 const Layout = (props:props) => {
     return (
         <div className="min-h-screen w-full bg-gray-100 flex flex-row overflow-auto">
-            <Sidebar close={props.sidebar} />
+            <Sidebar close={ props.sidebar } />
             <div className="flex-1 overflow-auto h-screen">
-                <Navbar title={props.header} />
-                <div className={!props.sidebar ? `py-4 px-10`: "px-2"}>
-                    {props.children}
+                <Navbar title={ props.header } />
+                <div className={ !props.sidebar ? `py-4 px-10`: "px-2" }>
+                    { props.children }
                 </div>
             </div>
 
