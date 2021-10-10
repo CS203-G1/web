3c4e0740dd99e16afb5b0915242ad76e173440e8
Rui Xian <tayrx9988@gmail.com>
2021-10-10 16:49:50 +0800

    add space in EmployeeRosterItem.tsx

--- components/Roster/EmployeeRosterItem.tsx
+++ components/Roster/EmployeeRosterItem.tsx
@@ -11,7 +11,7 @@ const EmployeeRosterItem = (props: props) => {
         <div className="grid grid-cols-8 bg-white border border-gray-300">
             <div className="flex-1 flex flex-row pl-4 gap-2 border border-gray-300 items-center">
                 <div className="h-10 w-10">
-                    <img className="rounded-full object-cover" src={props.photoUrl} alt=""/>
+                    <img className="rounded-full object-cover" src={props.photoUrl} alt="" />
                 </div>
 
                 <p className="font-semibold text-gray-500">
