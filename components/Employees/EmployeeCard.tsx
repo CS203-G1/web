760e032b063e1f00ba3a722731c9cc6b43ecac12
Rui Xian <tayrx9988@gmail.com>
2021-10-10 16:48:37 +0800

    add space to EmployeeCard.tsx

--- components/Employees/EmployeeCard.tsx
+++ components/Employees/EmployeeCard.tsx
@@ -17,20 +17,20 @@ const EmployeeCard = (props: Employee) => {
         "Not Tested": "bg-red-500"
     }
     return (
-        <Link href={`/employee/${props.id}`}>
+        <Link href={`/employee/${ props.id }`}>
             <div className="flex flex-col gap-2 w-72 pt-6 bg-white items-center rounded-lg shadow cursor-pointer transform scale-100 hover:shadow-lg hover:scale-105 duration-300 ease-in-out">
                 <div className="rounded-full h-32 w-32">
-                    <img className="rounded-full object-cover" src="https://picsum.photos/200" alt=""/>
+                    <img className="rounded-full object-cover" src="https://picsum.photos/200" alt="" />
                 </div>
                 <h3 className="text-lg font-semibold">
-                    {props.name}
+                    { props.name }
                 </h3>
                 <p className="text-gray-500 font-semi-bold">
-                    {props.email}
+                    { props.email }
                 </p>
                 <div className="flex flex-row gap-2 items-center">
                     <span className={`p-1 ${color[props.status]} rounded-full`}></span>
-                    <span>{props.status}</span>
+                    <span>{ props.status }</span>
                 </div>
                 <div className="flex flex-row gap-2 w-full px-10 items-center justify-center">
                         <UilClock height={20} width={70} color="#4b5563" />
@@ -39,7 +39,7 @@ const EmployeeCard = (props: Employee) => {
                         <Progress percent={30} strokeWidth={5} showInfo={false} />
                 </div>
                 <div className="py-4 bg-gray-100 w-full border-t text-center text-gray-500 font-semibold">
-                    {props.position}
+                    { props.position }
                 </div>
             </div>
         </Link>
