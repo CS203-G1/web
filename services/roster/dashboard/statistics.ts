9fec54a4f92f2ee5834c36cb04d7b2f778464dfe
Rui Xian <tayrx9988@gmail.com>
2021-10-12 18:19:02 +0800

    add url to services

--- services/roster/dashboard/statistics.ts
+++ services/roster/dashboard/statistics.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { DashboardSummary }from '../../../types/dashboard/summary'
 
-const uri = process.env.NEXT_PUBLIC_ROSTER_URL
+const uri = "https://ba8cm5du31.execute-api.ap-southeast-1.amazonaws.com/dev"
 
 export const getEmployeesSummary = async ( accessToken: string ) => {
 
