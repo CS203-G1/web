4a05ce8f437f20b665c9ebf14a701d437994595c
Rui Xian <tayrx9988@gmail.com>
2021-10-13 09:57:37 +0800

    change uri

--- services/employees/employees.ts
+++ services/employees/employees.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { Employee } from '../../types/Employee/employee'
 
-const uri = "https://ba8cm5du31.execute-api.ap-southeast-1.amazonaws.com/dev"
+const uri = process.env.NEXT_PUBLIC_ROSTER_URL
 
 export const getAllEmployees = async ( accessToken: string ) => {
     const res = await axios.get<Employee[]>(`${uri}/roster/companies/14f671b0-511e-43e4-86bb-6828f7a8e12d/employees`, {
