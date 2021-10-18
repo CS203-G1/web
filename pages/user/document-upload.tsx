d1b4bab3409137bd041363c7b7027f889f9c4627
Rui Xian <tayrx9988@gmail.com>
2021-10-18 22:52:42 +0800

    add upload component from antd

--- pages/user/document-upload.tsx
+++ pages/user/document-upload.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import Layout from "../../components/Common/Layout"
+import { Upload, message } from 'antd';
+
+const { Dragger } = Upload;
 
 
 const DocumentUpload = () => {
