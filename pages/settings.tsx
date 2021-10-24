3ee3d6bd489f4a41b133e36389741783196984e5
Rui Xian <tayrx9988@gmail.com>
2021-10-24 09:51:25 +0800

    add user settings page

---
+++ pages/settings.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/Common/Layout"
+
+const Settings = () => {
+    return (
+        <Layout header="User Settings">
+
+        </Layout>
+    )
+}
+
+export default Settings
