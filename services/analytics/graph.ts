55b3b5bf47b8dbf94dbdbbcd94e0719f5ade61fc
Rui Xian <tayrx9988@gmail.com>
2021-11-03 17:47:09 +0800

    create service to call analytics service

--- services/analytics/graph.ts
+++ services/analytics/graph.ts
@@ -2,4 +2,6 @@ import axios from 'axios'
 
 const uri = process.env.NEXT_PUBLIC_ROSTER_URL
 
-export const 
+export const getAnalyticsBarGraph = async () => {
+    const res = axios.get(`${uri}/analytics/bar`)
+}
