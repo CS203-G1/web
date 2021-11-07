09a9fde9f2cc1c7c20a3e4031e0462c32f176247
Rui Xian <tayrx9988@gmail.com>
2021-11-07 14:47:12 +0800

    2 graphs

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ const Home: NextPage = () => {
     useEffect(() => {
         getAnalyticsBarGraph().then(res => {
             console.log(res);
-            
+
             setBar(res)
             setBarStats(res[0])
         })
@@ -26,13 +26,13 @@ const Home: NextPage = () => {
         <div>
             <div className="flex flex-row justify-between px-10 py-5 border-b items-center">
                 <div className="flex flex-row gap-3 items-center">
-                
+
                     <h1 className="text-2xl text-blue-900 font-semibold">
                         Dash
                     </h1>
                 </div>
 
-                <button className="px-4 py-2 rounded-md border hover:bg-blue-900 hover:text-white" onClick={() => {router.push("/login")}}>
+                <button className="px-4 py-2 rounded-md border hover:bg-blue-900 hover:text-white" onClick={() => { router.push("/login") }}>
                     Login
                 </button>
             </div>
@@ -49,8 +49,24 @@ const Home: NextPage = () => {
                     <ActivityCard value={0} type={"Total Deaths"} change={-1} />
                 </div>
 
-                <div className="w-full border rounded-md">
-                    <LineChartComponent data={bar.reverse().slice(0)} />
+                <div className="w-full grid lg:grid-cols-2 grid-cols-1 gap-2">
+                    <div>
+                        <h1 className="text-2xl mb-2">
+                            Vaccination Statistics
+                        </h1>
+                        <div className="w-full border p-1 rounded-md">
+                            <LineChartComponent data={bar.slice(0).reverse()} />
+                        </div>
+                    </div>
+
+                    <div>
+                        <h1 className="text-2xl mb-2">
+                            Vaccination Statistics
+                        </h1>
+                        <div className="w-full border p-1 rounded-md">
+                            <LineChartComponent data={bar.slice(0).reverse()} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
